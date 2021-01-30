85dca0107474d6ccd31663d3b82302847ab36a75
ChenHuayu <chenhuay17@mails.tsinghua.edu.cn>
2021-01-30 20:10:36 +0800

    2
    2

--- game/src/extraSettings.js
+++ game/src/extraSettings.js
@@ -8,7 +8,7 @@ let wuDi = true;
 let firstFruit = 0;
 
 // 让水果更 Q 弹：false 改为大于 0 小于 1 的任意小数（推荐 0.9）
-let fruitQTan = false;
+let fruitQTan = 0.3;
 
 // 让水果下落缓慢：false 改为大于 0 的任意数，值越大阻力越大，下落越慢（推荐 5）
 let fruitSlowDown = false;
